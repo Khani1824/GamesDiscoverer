647365d26474e96f8829b9fe054a99054daeb449
Khani1824 <KhanimamboMaluleke@gmail.com>
2025-03-10 14:18:37 +0200

    This prints out a list of cities.

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,10 @@
 // PascalCasing
 function Message(){
-    return <h1>Hello World</h1>;
+    // JSX: JavaScript XML
+    const name = 'Khani';
+    if(name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
 }
 
 export default Message;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,16 @@
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;

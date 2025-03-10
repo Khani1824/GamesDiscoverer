45303a22a04843b51ed23b84d2ac76e4c9934e42
Khani1824 <KhanimamboMaluleke@gmail.com>
2025-03-10 14:27:01 +0200

    Printing out a paragrapgh when the list is empty, without code duplication

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,18 +2,10 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
 
-  if (items.length == 0)
-    // returning a paragraph
-    return (
-      <>
-        <h1>List</h1>
-        <p>No item found</p>
-      </>
-    );
-
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No items found</p> : null}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>

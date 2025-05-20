6bf1e9a1e7119fd3fa424fc2f847032b059a94ef
Khani1824 <KhanimamboMaluleke@gmail.com>
2025-05-20 16:26:37 +0200

    Complete selectable list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+// We want to be able to pass {items: [], heading: string}
+interface Props {
+  items: string[];
+  heading: string;
+  // Function that takes parameters of type string (itme: string) and returns void
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
   // Hook: function that allows us to tap into built in features in react
   // This is a state Hook
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(-1); // How we tell rect that out componete cant have state
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
@@ -23,6 +30,7 @@ function ListGroup() {
             // Making each item a clickable item.
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
